55084e9ea93636d1f6ac3f7d38ce61f327b8a68a
Cleyton Amorim <cleytonr.amorim@gmail.com>
2020-02-20 16:00:33 -0400

    msg

---
+++ ExemploDatetime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExemploDatetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt =  DateTime.Now;
+            string s  = dt.ToLongTimeString();
+            string s2 = dt.ToLongDateString(); 
+            Console.WriteLine(s);
+            Console.WriteLine(s2);
+        }
+    }
+}
